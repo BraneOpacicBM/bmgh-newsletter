f9d1fbcd36bf498a704528af5c466a8c29f83b2e
Brane Opacic <braneopacic@gmail.com>
2023-06-26 08:27:09 +0200

    add basic structure with ts and scss modules

---
+++ src/components/Newsletter/Newsletter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Newsletter.module.scss";
+
+export const Newsletter = () => {
+  return (
+    <div className={styles.newsletterBody}>Write your newsletter from here</div>
+  );
+};
